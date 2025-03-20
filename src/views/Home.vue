1ec2a6ece88d92f99c61b51110d3216b2d5b2476
Simon Krispin <simon.krispin@gmail.com>
2025-03-20 11:53:37 +0100

    don't

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to the Blog App</h1>
+    <p>Please login to create or view blog posts.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
